c8ef6e1a9e549f9c50a3aa9827fd7731328453df
Khomsun Singhirunnusorn <khomsun.si@gmail.com>
2013-09-17 20:19:15 -0400

    Create Databaselex.py

---
+++ Databaselex.py
@@ -0,0 +1,33 @@
+rom ply import *
+
+keywords = (
+    'SET','GET','UNSET','END','NUMEQUALTO','BEGIN','ROLLBACK','COMMIT'
+)
+
+tokens = keywords + (
+     'ID','NUMBER','NEWLINE'
+)
+
+t_ignore = ' \t'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    if t.value in keywords:
+        t.type = t.value
+    return t
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_NEWLINE(t):
+    r'\n'
+    t.lexer.lineno += 1
+    return t
+
+def t_error(t):
+    print("Illegal character %s" % t.value[0])
+    t.lexer.skip(1)
+
+lex.lex(debug=0)
